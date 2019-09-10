0972c01f8108d586f55f6eb27c113417a983ea95
ccuGabinete <ccugabinete@gmail.com>
2019-09-10 15:14:15 -0300

    pequeno manejo

--- controllers/acessoController.js
+++ controllers/acessoController.js
@@ -6,93 +6,90 @@ const hash = require('../hash/hash');
 const colunaSenha = 'C3';
 const doc = new GoogleSpreadsheet(spreedsheetId);
 
-var sendJsonResponse = function(res, status, content){
+var sendJsonResponse = function (res, status, content) {
     res.status(status);
     res.json(content);
 }
 
 
-module.exports.acessarPlanilha = async() => {
-    try
-    {
-        const doc =  new GoogleSpreadsheet(spreedsheetId);
+module.exports.acessarPlanilha = async () => {
+    try {
+        const doc = new GoogleSpreadsheet(spreedsheetId);
         await promisify(doc.useServiceAccountAuth)(credentials);
         return await promisify(doc.getInfo)();
-            
+
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
-   
+
 }
 
 module.exports.buscarId = async (req, res, next) => {
-      
+
     const info = await this.acessarPlanilha();
     const folhaDeDados = info.worksheets[0]
     const linhas = await promisify(folhaDeDados.getRows)({
         query: 'id = ' + req.body.id.toString()
     })
 
-    res.json(linhas);        
+    res.json(linhas);
 }
 
 module.exports.validarSenha = async (req, res, next) => {
-      
+
     const info = await this.acessarPlanilha();
     const folhaDeDados = info.worksheets[0]
     const linhas = await promisify(folhaDeDados.getRows)({
         query: 'login = ' + req.body.login
     })
 
+    console.log(linhas);
+
     var obj = {};
 
-    if(linhas){
-        if(linhas.length > 0){
-            obj = {
-                    'nome': linhas[0].nome,
-                    'link': linhas[0].link
-                }
+
+    if (linhas.length > 0) {
+        obj = {
+            'nome': linhas[0].nome,
+            'link': linhas[0].link
         }
 
-        if(linhas.length > 0){
-            const hashSenha = linhas[0].senha;  
-            var resposta = await hash.validarSenha(req.body.senha, hashSenha);
-            obj['isValid'] = resposta;
-            sendJsonResponse(res, 200, obj)
-        }else {
-            obj['isValid'] = false;
-            obj['nome'] = '';
-            obj['link'] = '';
-            sendJsonResponse(res, 200, obj)
-        }
-    }else{
-        sendJsonResponse(res, 500, obj)
-    }   
+        const hashSenha = linhas[0].senha;
+        var resposta = await hash.validarSenha(req.body.senha, hashSenha);
+        obj['isValid'] = resposta;
+        sendJsonResponse(res, 200, obj)
+    } else {
+        obj['isValid'] = false;
+        obj['nome'] = '';
+        obj['link'] = '';
+        sendJsonResponse(res, 401, obj)
+    }
+
 }
 
 
 module.exports.atualizarSenha = async (req, res, next) => {
-      
+
     this.login = req.body.login;
     this.senha = req.body.senha;
-   
+
     const info = await this.acessarPlanilha();
     const folhaDeDados = info.worksheets[0];
     const linhas = await promisify(folhaDeDados.getCells)({});
- 
+
     let filter = (celula) => {
         return celula.value == this.login;
     }
 
     let celula = linhas.filter(filter)[0];
 
-    if(celula){
-        let linhaEncontrada = celula.batchId.substring(0,2)
-    
+    if (celula) {
+        let linhaEncontrada = celula.batchId.substring(0, 2)
+
         let batchId = linhaEncontrada + colunaSenha;
         console.log(batchId);
-        
+
 
         let filterSenha = (celula) => {
             return celula.batchId == batchId;
@@ -100,15 +97,15 @@ module.exports.atualizarSenha = async (req, res, next) => {
 
         let celulaLogin = linhas.filter(filterSenha)[0]
         console.log(celulaLogin);
-           
+
         celulaLogin.value = this.senha;
-        celulaLogin.save();   
+        celulaLogin.save();
         folhaDeDados.bulkUpdateCells(linhas);
         res.json(0);
-        
-    }else{
+
+    } else {
         res.json(-1);
     }
-        
+
 }
 

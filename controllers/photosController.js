d19c09d53b96596fa877f4c556fe09add61395f3
ccuGabinete <ccugabinete@gmail.com>
2019-09-18 18:50:52 -0300

    termino do endpoint para envio de imagens

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -1,47 +1,64 @@
 const credentials = require('../credentials.json');
 const fs = require('fs');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const util = require('util');
 const promisify = util.promisify;
-const folderId = '1K_f3NvyCe1I59rzjN6FoCcf1cPBYAClt';
-const promise = require('q');
+const folderId = '1Prkv4TK-dB-UvXzk1vsqbKi2a9tPDSD7';
+const base64 = require('base64-img')
 
 const scopes = [
-    'https://www.googleapis.com/auth/drive'
-  ];
+  'https://www.googleapis.com/auth/drive'
+];
 
-  const auth = new google.auth.JWT(
-    credentials.client_email, null,
-    credentials.private_key, scopes
-  );
-
-  const drive =  google.drive({version: 'v3', auth});
-  const salvarFoto = promisify(drive.files.create)
-    
-  const salvarSync = async () => {
-      
-    var fileMetadata = {
-      'name':'teste2.png',
-      parents: [folderId]
-    };
-    var media = {
-      mimeType: 'image/png',
-      body: fs.createReadStream('../ti.png')
-    };
-    
-    try{
-
-        const teste = await drive.files.create({
-            resource: fileMetadata,
-            media: media,
-            fields: 'id'
-        });
-
-        console.log(teste.data);
-    
-    }catch(err){
-        console.log(err);
-    }
+var sendJsonResponse = function (res, status, content) {
+  res.status(status);
+  res.json(content);
 }
 
-salvarSync()
+
+const auth = new google.auth.JWT(
+  credentials.client_email, null,
+  credentials.private_key, scopes
+);
+
+const drive = google.drive({ version: 'v3', auth });
+const converterFoto = promisify(base64.img)
+
+const salvarSync = async (res, nome) => {
+
+  var fileMetadata = {
+    'name': nome,
+    parents: [folderId]
+  };
+  var media = {
+    mimeType: 'image/png',
+    body: fs.createReadStream(__dirname + '/public/asserts/teste.jpg')
+  };
+
+  try {
+
+    const response = await drive.files.create({
+      resource: fileMetadata,
+      media: media,
+      fields: 'id'
+    });
+
+    sendJsonResponse(res, 200, 'https://drive.google.com/uc?id=' + response.data.id)
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports.salvarImagem = async (req, res, next) => {
+
+  converterFoto(req.body.base64, __dirname + '/public/asserts', 'teste')
+    .then((filepath) => {
+      salvarSync(res, req.body.nome);
+    })
+
+    .catch((err) => {
+      sendJsonResponse(res, 500, { error: err })
+    })
+}
+
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,10 @@ var router = express.Router();
 var GoogleSpreadsheet = require('google-spreadsheet');
 const google = require('../controllers/googleController');
 const acesso = require('../controllers/acessoController');
-const fotos = require('../controllers/teste')
-const { promisify } = require('util')
-const credentials = require('../credentials.json')
+const fotos = require('../controllers/photosController')
 const spreedsheetId = '1crH8vAK_E8wK7Vt8IDy8xN772HENebuXZoBicwmT_ew';
-const doc = new GoogleSpreadsheet(spreedsheetId);
 const ping = require('../controllers/pingController');
-var mid = require('../asyncMiddleware')
+var mid = require('../asyncMiddleware');
 
 router.get('/listarTodas', mid.asyncMiddleware(google.listarTodas))
 router.post('/', mid.asyncMiddleware(google.salvar))
@@ -19,8 +16,7 @@ router.post('/buscarDoc', mid.asyncMiddleware(google.buscarDoc))
 router.post('/acesso/validar', mid.asyncMiddleware(acesso.validarSenha))
 router.post('/acesso/atualizarSenha', mid.asyncMiddleware(acesso.atualizarSenha))
 router.post('/acesso/salvar', mid.asyncMiddleware(acesso.salvar))
-router.get('/salvarFoto', mid.asyncMiddleware(fotos.salvarImagem))
+router.post('/salvarFoto', mid.asyncMiddleware(fotos.salvarImagem))
 router.get('/ping', ping.ping)
 
 module.exports = router
-

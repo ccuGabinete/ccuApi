c4dddfa9753d95a5e05ef756d10c459736190066
ccugabiete <ccugabinete@gmail.com>
2020-03-04 02:41:19 -0300

    sacanagem do iplan - consertei

--- controllers/sccaController.js
+++ controllers/sccaController.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const iconv = require('iconv-lite');
 URL_OF_WEBSITE = 'http://scca.rio.rj.gov.br/index.php/online?im=';
-var sendJsonResponse = function (res, status, content) {
+var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 }
@@ -22,10 +22,10 @@ module.exports.getScca = (req, res, next) => {
     };
     console.log(req.body);
     request.get({
-        uri: URL_OF_WEBSITE + req.body.matricula,
-        encoding: null
-    },
-        function (err, resp, body) {
+            uri: URL_OF_WEBSITE + req.body.matricula,
+            encoding: null
+        },
+        function(err, resp, body) {
             if (err) {
                 sendJsonResponse(res, 500, err)
             } else {
@@ -33,28 +33,30 @@ module.exports.getScca = (req, res, next) => {
                 let bodyText = JSON.stringify(bodyWithCorrectEncoding);
                 if (bodyText.length < 1000) {
                     sendJsonResponse(res, 400, notificado);
-                } else {                   
-                   
-                    let result = bodyText.match(/<span>(.*?)<\/span>/g).map(function (val) {
+                } else {
+
+                    let result = bodyText.match(/<span>(.*?)<\/span>/g).map(function(val) {
                         let campos = val.replace(/<\/?span>/g, '');
                         return campos;
                     });
+
+
 
                     notificado['matricula'] = result[0];
                     notificado['concessao'] = result[1];
                     notificado['isento'] = result[2];
                     notificado['equipamento'] = result[3];
                     notificado['local'] = result[4];
-                    notificado['situacao'] = result[5];
-                    notificado['nome'] = result[6];
-                    notificado['cpf'] = result[7];
+                    notificado['situacao'] = result[6];
+                    notificado['nome'] = result[7];
+                    notificado['cpf'] = result[8];
 
-                    if (result.length < 11) {
+                    if (result.length < 12) {
                         notificado['nomeaux'] = '';
                         notificado['cpfaux'] = '';
                     } else {
-                        notificado['nomeaux'] = result[8];
-                        notificado['cpfaux'] = result[9];
+                        notificado['nomeaux'] = result[9];
+                        notificado['cpfaux'] = result[10];
                     }
 
                     sendJsonResponse(res, 200, notificado)
